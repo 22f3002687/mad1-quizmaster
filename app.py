6f041c2be07ad6bb2506e76ac6d46307bb58d518
22f3002687 <22f3002687@ds.study.iitm.ac.in>
2025-02-26 13:14:28 +0530

    adding requirements file and main application file

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from models import *
+from dotenv import load_dotenv
+import os
+from controllers import auth_bp, login_manager
+
+def create_app():
+    app = Flask(__name__)
+
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quiz_master.db"
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        admin = Admin.query.filter_by(id=1).first()
+        if not admin:
+            admin = Admin(username="Adminstartor", email='admin@quizmaster.com')
+            admin.set_password('admin@#2468')
+            db.session.add(admin)
+            db.session.commit()
+
+    app.register_blueprint(auth_bp)
+    login_manager.init_app(app)
+
+    
+    
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
